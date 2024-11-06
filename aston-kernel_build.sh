41a119c5ee5bbb7c76e6e461f29e401407e46067
Shandorman <98683030+jiganomegsdfdf@users.noreply.github.com>
2024-11-06 08:24:16 +0300

    Update aston-kernel_build.sh

--- aston-kernel_build.sh
+++ aston-kernel_build.sh
@@ -9,7 +9,7 @@ sed -i "s/Version:.*/Version: ${_kernel_version}/" $1/linux-oneplus-aston/DEBIAN
 cat $1/linux/arch/arm64/boot/Image $1/linux/arch/arm64/boot/dts/qcom/sm8550-oneplus-aston.dtb > $1/linux/Image_w_dtb
 gzip Image_w_dtb
 chmod +x $1/mkbootimg
-$1/mkbootimg --header_version 4 --base 0x0 --kernel $1/linux/Image_w_dtb.gz -o boot.img
+$1/mkbootimg --header_version 4 --base 0x0 --kernel $1/linux/Image_w_dtb.gz -o $1/boot.img
 
 make -j$(nproc) ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- INSTALL_MOD_PATH=$1/linux-oneplus-aston modules_install
 rm $1/linux-oneplus-aston/lib/modules/**/build
